adff63b7e868e7b20d406c5ac70596cf1fa2504f
campussquirrel <fadaei@uni-hildesheim.de>
2018-06-07 17:31:18 +0200

    reading json elements

--- readingAllFiles.py
+++ readingAllFiles.py
@@ -7,22 +7,30 @@ import subprocess
 from elasticsearch import Elasticsearch
 #subprocess.Popen('C:/Users/Noushin/Downloads/elasticsearch-6.2.4/elasticsearch-6.2.4/bin/elasticsearch.bat')
 es = Elasticsearch()
-es.reindex
-#es.indices.refresh(index="myindex")
+#es.reindex
+#es.indices.refresh(index="myindex1")
 #es.indices.create(index='myindex6')
 #sys.stdout=open("RESULT.txt","w")
 f = open('RESULT', 'a', encoding='utf-8')
 i=0
-for dirpath, dirs, files in os.walk("TestSamples/Data/"):
+#data=[]
+for dirpath, dirs, files in os.walk("Data/"):
     for filename in files:
         fname = os.path.join(dirpath, filename)
         if(fname.endswith("json")):
             with open(fname, encoding="utf8") as myfile:
                 print(fname)
+                #data = json.load(myfile)
                 for line in myfile.readlines():
                     if(line.__contains__("{\"index\":{")):
                         continue
-                    myres = es.index(index="myindex", doc_type='patent', id=i+1, body=line)
+                    dataObject=json.loads(line)
+                    for item in dataObject:
+                        print(dataObject[item])
+                        #if not dataObject[item]:
+                        #    print('Not!')
+
+                    myres = es.index(index="myindex1", doc_type='patent', id=i+1, body=line)
                     i=i+1
                     #f.write(line)
 
@@ -30,7 +38,7 @@ f.write(myres['result'])
 f.close()
 print(myres['result'])
 
-myres1=es.search(index="myindex",q='PRYF="1986"')
+myres1=es.search(index="myindex1",q='PRYF="2003"')
 print("Got %d Hits:" % myres1['hits']['total'])
 for hit in myres1['hits']['hits']:
     #print(hit)
